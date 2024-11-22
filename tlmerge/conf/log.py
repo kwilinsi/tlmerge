ebb37314604d2a371d6f05a4c88e8bcb56cf1c16
kwilinsi <kwilinsi.314@gmail.com>
2024-11-22 16:29:39 -0500

    Always log DEBUG messages to file

--- tlmerge/conf/log.py
+++ tlmerge/conf/log.py
@@ -21,38 +21,15 @@ def configure_log(file: Path | None,
     # Configure the root logger
     root = logging.getLogger()
 
-    # Add log file if enabled
-    to_file = False
-    if file is not None:
-        to_file = True
-        # Make sure it's not a directory. Generally this was validated earlier,
-        # but that may not have occurred if the log file was specified in the
-        # global config file
-        if file.is_dir():
-            sys.exit(1 if console == 'silent' else
-                     f"Invalid log file \"{file}\": that's a directory")
+    # Capture warnings in logger
+    logging.captureWarnings(True)
 
-        # Ensure directory exists
-        file.parent.mkdir(parents=True, exist_ok=True)
-
-        # Add file handler to root
-        file_handler = logging.handlers.RotatingFileHandler(
-            file,
-            maxBytes=1024 * 1024,  # 1 MB
-            backupCount=5,
-            encoding='utf-8'
-        )
-        file_handler.setFormatter(logging.Formatter(
-            fmt='%(asctime)s.%(msecs)03d - '
-                '%(threadName)-15.15s %(name)-20.20s %(levelname)-8s '
-                '%(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S'
-        ))
-        root.addHandler(file_handler)
-
-    # Add console handler (with color) if enabled
-    to_console = False
-    if console != 'silent':
+    if console == 'silent':
+        # Suppress all console messages
+        root.setLevel(logging.CRITICAL + 1)
+        to_console = False
+    else:
+        # Add console handler (with color)
         to_console = True
         coloredlogs.install(
             level=(logging.DEBUG if console == 'verbose'
@@ -76,17 +53,46 @@ def configure_log(file: Path | None,
             },
         )
 
+        # Show ALL warnings (incl. duplicates) in verbose mode
+        if console == 'verbose':
+            warnings.simplefilter('always')
+
+    # Add log file if enabled
+    to_file = False
+    if file is not None:
+        # Set root level to ensure all DEBUG messages to go log file
+        root.setLevel(logging.DEBUG)
+
+        to_file = True
+        # Make sure it's not a directory. Generally this was validated earlier,
+        # but that may not have occurred if the log file was specified in the
+        # global config file
+        if file.is_dir():
+            sys.exit(1 if console == 'silent' else
+                     f"Invalid log file \"{file}\": that's a directory")
+
+        # Ensure directory exists
+        file.parent.mkdir(parents=True, exist_ok=True)
+
+        # Add file handler to root
+        file_handler = logging.handlers.RotatingFileHandler(
+            file,
+            maxBytes=1024 * 1024,  # 1 MB
+            backupCount=5,
+            encoding='utf-8'
+        )
+
+        file_handler.setFormatter(logging.Formatter(
+            fmt='%(asctime)s.%(msecs)03d - '
+                '%(threadName)-15.15s %(name)-20.20s %(levelname)-8s '
+                '%(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        ))
+
+        root.addHandler(file_handler)
+
     # Suppress debug messages from PIL that spam the logs with EXIF data
     logging.getLogger('PIL').setLevel(logging.INFO)
-
-    # Capture warnings in logger
-    logging.captureWarnings(True)
-
-    # Adjust warning filter based on log level
-    if console == 'silent':
-        warnings.simplefilter('ignore')
-    elif console == 'verbose':
-        warnings.simplefilter('always')
 
     # Log a success message
     if to_file and to_console:
