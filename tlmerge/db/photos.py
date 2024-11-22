ca9191a3bc8e14ee7e1727e9f9f5b68bdbcbf462
kwilinsi <kwilinsi.314@gmail.com>
2024-11-21 22:32:41 -0500

    Clean up unused import

--- tlmerge/db/photos.py
+++ tlmerge/db/photos.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import (Boolean, DateTime, Float, ForeignKey,
-                        Integer, String, UniqueConstraint)
+from sqlalchemy import Boolean, DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
