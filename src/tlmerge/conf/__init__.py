abb08136e5e31f09c13720edd571a83c746e8718
kwilinsi <kwilinsi.314@gmail.com>
2025-02-11 15:45:45 -0500

    Remove * imports

--- src/tlmerge/conf/__init__.py
+++ src/tlmerge/conf/__init__.py
@@ -1,5 +1,6 @@
 from .cli_args import parse_cli
 from .config import LogLevel, DateConfig, GroupConfig, RootConfig
-from .const import *
+from .const import (DEFAULT_DATABASE_FILE, DEFAULT_CONFIG_FILE,
+                    DEFAULT_LOG_FILE, ENV_VAR_PREFIX)
 from .log import buffer_console_log, configure_log, LogBuffer, LogLevel
 from .manager import ConfigManager, write_default_config
--- src/tlmerge/db/__init__.py
+++ src/tlmerge/db/__init__.py
@@ -1,4 +1,6 @@
 from .cameras import Camera
 from .db import DB
-from .lenses import *
-from .photos import *
+from .lenses import (Lens, MAX_LENS_MAKE_LENGTH,
+                     MAX_LENS_MODEL_LENGTH, MAX_LENS_SPEC_LENGTH)
+from .photos import (MAX_DATE_LENGTH, MAX_GROUP_LENGTH,
+                     MAX_PHOTO_NAME_LENGTH, Photo)
