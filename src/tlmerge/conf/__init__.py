3bd33497fb092db59d646aef2f63cbea00f9569d
kwilinsi <kwilinsi.314@gmail.com>
2025-02-12 23:09:08 -0500

    Move config models and enums to structs module

--- src/tlmerge/conf/__init__.py
+++ src/tlmerge/conf/__init__.py
@@ -1,6 +1,6 @@
 from .cli_args import parse_cli
-from .config import (LogLevel, BaseConfig, DateConfig, GroupConfig,
-                     RootConfig, ThumbLocation)
+from .config import LogLevel, BaseConfig, DateConfig, GroupConfig, RootConfig
+from .config_structs import ThumbLocation
 from .const import (DEFAULT_DATABASE_FILE, DEFAULT_CONFIG_FILE,
                     DEFAULT_LOG_FILE, ENV_VAR_PREFIX)
 from .log import buffer_console_log, configure_log, LogBuffer, LogLevel
--- src/tlmerge/conf/config.py
+++ src/tlmerge/conf/config.py
@@ -4,25 +4,23 @@ from abc import ABC, abstractmethod
 from collections.abc import Callable, Iterable
 from copy import copy
 import csv
-from enum import Enum
 import inspect
 from io import StringIO
 import logging
 import os
 from pathlib import Path
 import re
-from typing import Annotated, Any, Literal, Self, TypeAlias
+from typing import Annotated, Any, Literal, Self
 
-from pydantic import (AfterValidator, AliasChoices, BaseModel, BeforeValidator,
-                      ConfigDict, Field, validate_call)
+from pydantic import (AfterValidator, BeforeValidator, ConfigDict,
+                      Field, validate_call)
 
 from .const import ENV_VAR_PREFIX, DEFAULT_DATABASE_FILE, DEFAULT_LOG_FILE
 from .log import LogLevel
+from .config_structs import (ChromaticAberrationModel, ThumbLocation,
+                             WhiteBalanceModel, WhiteBalanceType)
 
 _log = logging.getLogger(__name__)
-
-WhiteBalanceType: TypeAlias = tuple[float, float, float, float] | \
-                              Literal['auto', 'camera', 'default']
 
 # This is the main config used for validating most types. It ignores excess
 # whitespace in string values, but that's it. Strings are still case-sensitive.
@@ -34,84 +32,6 @@ MAIN_PYDANTIC_CONFIG = ConfigDict(str_strip_whitespace=True)
 # like a file name, where it is case-sensitive.
 STRING_PYDANTIC_CONFIG = ConfigDict(str_to_lower=True,
                                     str_strip_whitespace=True)
-
-
-class WhiteBalanceModel(BaseModel):
-    model_config = ConfigDict(extra='forbid')
-
-    # Note that green_2 shares the green_1 aliases, meaning that if you omit
-    # green_2, it'll just use the same values as green_1
-    red: Annotated[float, Field(ge=0, validation_alias=AliasChoices(
-        'r', 'red'))]
-    green_1: Annotated[float, Field(ge=0, validation_alias=AliasChoices(
-        'g1', 'g', 'green_1', 'green1', 'green'))]
-    blue: Annotated[float, Field(ge=0, validation_alias=AliasChoices(
-        'b', 'blue'))]
-    green_2: Annotated[float, Field(ge=0, validation_alias=AliasChoices(
-        'g2', 'green_2', 'green2', 'green', 'g',
-        'green_1', 'green1', 'g1'))]
-
-    @classmethod
-    def to_tuple(
-            cls,
-            data: WhiteBalanceType | WhiteBalanceModel) -> WhiteBalanceType:
-        """
-        If the given white balance data is a WhiteBalanceModel, convert it to a
-        tuple of four floats in RGBG order. Otherwise, return it unmodified.
-
-        :return: The white balance data.
-        """
-
-        if isinstance(data, WhiteBalanceModel):
-            return data.red, data.green_1, data.blue, data.green_2
-
-        return data
-
-
-class ChromaticAberrationModel(BaseModel):
-    model_config = ConfigDict(extra='forbid')
-    red: Annotated[float, Field(ge=0, validation_alias=AliasChoices(
-        'r', 'red'))]
-    blue: Annotated[float, Field(ge=0, validation_alias=AliasChoices(
-        'b', 'blue'))]
-
-    @classmethod
-    def to_tuple(cls,
-                 data: tuple[float, float] | ChromaticAberrationModel) -> \
-            tuple[float, float]:
-        """
-        If the given chromatic aberration data is a ChromaticAberrationModel,
-        convert it to a tuple of two floats, red and blue, in that order.
-        Otherwise, return it unmodified.
-
-        :return: The chromatic aberration data.
-        """
-
-        if isinstance(data, ChromaticAberrationModel):
-            return data.red, data.blue
-
-        return data
-
-
-class ThumbLocation(Enum):
-    """
-    These are the possible settings for where thumbnails can be stored.
-    For each option except CUSTOM, thumbnails are stored in some folder (by
-    default "thumb") at the specified location.
-
-    Consider the raw picture "2000-01-01/a/0001.dng". A thumbnail could be
-    stored in any of these locations:
-
-    - ROOT: `<PROJECT_ROOT>/thumb/2000-01-01/a/0001.jpg`
-    - DATE: `<PROJECT_ROOT>/2000-01-01/thumb/a/0001.jpg`
-    - GROUP: `<PROJECT_ROOT>/2000-01-01/a/thumb/0001.jpg`
-    - CUSTOM: `<SOME_CUSTOM_PATH>/2000-01-01/a/0001.jpg`
-    """
-
-    ROOT = 0
-    DATE = 1
-    GROUP = 2
-    CUSTOM = 3
 
 
 def coerce_float_tuple(raw: Any) -> Any:
